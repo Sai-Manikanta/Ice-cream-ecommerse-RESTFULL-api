2566f5e2a308e867dd2bf758dfd9b301ba16c767
mani333007 <mani333007@gmail.com>
2021-01-12 19:21:42 +0530

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// MongoDB connection
+mongoose.connect('mongodb://localhost/ice_creams_ecomerse', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('connected to Database')
+}).catch(err => {
+    console.log(`Error while connected to mongoDB: ${err}`)
+})
+
+// api routes
 const iceCreamsRoutes = require('./routes/iceCreams');
 
 app.use('/api/ice-creams',iceCreamsRoutes);
